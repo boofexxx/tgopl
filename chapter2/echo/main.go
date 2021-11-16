8cf09662d72656698bbfaac1211f256ef88225d9
boof <boofmaq@gmail.com>
2021-11-16 21:18:16 +0500

    chapter 2. echo. deleted unnecessary stuff

--- chapter2/echo/main.go
+++ chapter2/echo/main.go
@@ -10,11 +10,6 @@ var n = flag.Bool("n", false, "omit trailing newline")
 var sep = flag.String("s", " ", "separator")
 
 func main() {
-	for i := 0; i < 3; i++ {
-		c := 5
-		fmt.Printf("%p\n", &i)
-		fmt.Printf("%p\n", &c)
-	}
 	flag.Parse()
 	fmt.Print(strings.Join(flag.Args(), *sep))
 	if !*n {
